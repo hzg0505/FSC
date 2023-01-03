83f5fcab90e5136a3c76fa35caccc4f2c5130e16
hzg0505 <hzg0505@163.com>
2023-01-03 23:33:01 +0800

    attn

--- networks/net.py
+++ networks/net.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 import copy
 from networks.net_units import get_activation, build_backbone
 
+
 class Exemplar_scale_aug(nn.Module):
     def __init__(self, backbone, embed_dim, match_dim, out_stride=8, max_stride=32, exemplar_scales=[0.8, 1, 1.2]):
         super(Exemplar_scale_aug, self).__init__()
@@ -13,6 +14,9 @@ class Exemplar_scale_aug(nn.Module):
             nn.Conv2d(embed_dim, match_dim, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(match_dim),
             nn.ReLU(),
+            nn.Conv2d(match_dim, match_dim, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(match_dim),
+            nn.ReLU()
         )
         self.out_stride = out_stride
         self.max_stride = max_stride
@@ -39,7 +43,7 @@ class Exemplar_scale_aug(nn.Module):
             feat_box = feat[:, :, y_tl : (y_br + 1), x_tl : (x_br + 1)]
             feat_boxes.append(feat_box)
         return feat_boxes
-
+    
     def __call__(self, image, boxes):
         feat = self.backbone(image)
         feat = self.conv(feat)
@@ -75,28 +79,53 @@ class Exemplar_scale_aug(nn.Module):
             exemplars_list.append(feat_boxes)
 
         return feat, exemplars_list
-        
+
+def print_value(tensor, name):
+    print("【{}】|max:{:5.5f} ｜ min:{:2.5f} | mean:{:5.5f}".format(name, tensor.max(), tensor.min(), tensor.mean()))
+
 class Match(nn.Module):
     def __init__(self, head, head_dim):
         super().__init__()
         self.head = head
         self.head_dim = head_dim
+
+        self.conv_out = nn.Sequential(
+            nn.Conv2d(head_dim*2, head_dim, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(head_dim),
+            nn.ReLU()
+        )
+        self.norm = nn.LayerNorm([1, 64, 64])
     
     def forward(self, feat, exemplar):
         _, _, h_p, w_p = exemplar.size()
         _, _, h_f, w_f = feat.size()
         # feat: 1, c, h, w. exemplar: 1, c, h, w
-        feat = feat.contiguous().view(self.head, self.head_dim, h_f, w_f) # [head,c,h,w]
-        exemplar = exemplar.contiguous().view(self.head, self.head_dim, h_p, w_p) # [head,c,h,w]
+        feat = feat.contiguous().view(self.head, self.head_dim, h_f, w_f)         # [head,head_dim,h,w]
+        exemplar = exemplar.contiguous().view(self.head, self.head_dim, h_p, w_p) # [head,head_dim,h,w]
         pad = (w_p // 2, w_p // 2, h_p // 2, h_p // 2)
         attn_list = []
         for q, k in zip(feat, exemplar):
-            attn = F.conv2d(F.pad(q.unsqueeze(0), pad), k.unsqueeze(0))  # [1,1,h,w]
+            # 相似性度量。  卷积、注意力、距离
+            attn = F.conv2d(F.pad(q.unsqueeze(0), pad), k.unsqueeze(0))           # [1,1,h,w]
+            # print_value(attn, "attn0")
+            attn = self.norm(attn) 
+            attn = torch.sigmoid(attn)
             attn_list.append(attn)
-        attn = torch.cat(attn_list, dim=0)  # [head,1,h,w]
-        feat = torch.mul(feat, attn)
+        attn = torch.cat(attn_list, dim=0)                                        # [head,1,h,w]
+        attn_feat = torch.mul(feat, attn)                                         # [head,head_dim,h,w]
+        feat = torch.concat([attn_feat, feat], dim=1)                             # [head,head_dim*2,h,w]
+        feat = self.conv_out(feat)
+        feat = feat.contiguous().view(1, self.head_dim*self.head, h_f, w_f)       # [1, head*head_dim,h,w]
         return feat
 
+# 激活函数选择
+def get_activation(activation='relu'):
+    if activation=='relu':
+        return nn.ReLU()
+    elif activation=='leaky_relu':
+        return nn.LeakyReLU()
+    else:
+        raise NotImplementedError
 
 # 回归
 class Regressor(nn.Module):
@@ -147,19 +176,16 @@ class Model(nn.Module):
                                                      exemplar_scales=exemplar_scales)
         self.match = Match(head, head_dim)
         self.conv = nn.Sequential(
-            nn.Conv2d(head_dim, head_dim, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(head_dim),
-            nn.ReLU(),
+            nn.Conv2d(head_dim*head*2, head_dim*head, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(head_dim*head),
+            nn.ReLU()
         )
         self.conv_out = nn.Sequential(
-            nn.Conv2d(head_dim*head*2, head_dim, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(head_dim*head, head_dim, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(head_dim),
-            nn.ReLU(),
-            nn.Conv2d(head_dim, head_dim, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(head_dim),
-            nn.ReLU(),
+            nn.ReLU()
         )
-        self.regressor = Regressor(in_dim=head_dim,  activation='leaky_relu')
+        self.regressor = Regressor(in_dim=head_dim,  activation='relu')
         
     def forward(self, image, boxes):
         # image: 1,c,h,w
@@ -168,7 +194,8 @@ class Model(nn.Module):
         feat_orig, exemplars_list = self.exemplar_scale_aug(image, boxes_orig)
         # num_scale, num_boxes, feat. => M, K, 1,c,h',w'  => 3, 3(4), 1,c,h',w'
         match_feats_scale_shot_list = []
-        feat_match = feat_orig
+        feat_match = feat_orig     # 1, c, h, w
+#         print(feat_match.shape)
         for i in range(self.shot):            # 不同样本
             match_feats_scale_list = []
             for exemplars in exemplars_list:  # 不同尺度
@@ -179,19 +206,18 @@ class Model(nn.Module):
                     exemplar = nn.AdaptiveAvgPool2d(self.pool_cfg['size'])(exemplar)
                 else:
                     raise NotImplementedError
-                match_feat = self.match(feat_match, exemplar)   # [head, head_dim, h, w]
+                match_feat = self.match(feat_match, exemplar)   # [1, head*head_dim,h,w]
                 match_feats_scale_list.append(match_feat)
-            match_feats = torch.stack(match_feats_scale_list)   # [scales, head, head_dim, h, w]
-            feat_match = torch.sum(match_feats, dim=0)          # [head, head_dim, h, w]
-            feat_match = self.conv(feat_match)                  # [head, head_dim, h, w]
-        feat_match = feat_match.contiguous().view(1, self.head_dim*self.head, 64, 64) # [1,c*head,h,w]
-        feat = torch.concat([feat_orig, feat_match])
-        feat = feat.contiguous().view(1, self.head_dim*self.head*2, 64, 64) # [1,c*head,h,w]
-        feat = self.conv_out(feat)
-        feat = self.regressor(feat)
+            match_feats = torch.stack(match_feats_scale_list)   # scales, 1, c, h, w, 
 
+            feat_match = torch.concat([torch.sum(match_feats, dim=0), feat_match], dim=1)  # 1, 2*c, h, w. 
+            feat_match = self.conv(feat_match)                  # 1, c, h, w. [head, head_dim, h, w]
+        feat = self.conv_out(feat_match)
+        feat = self.regressor(feat)
+#         print("feat:{}".format(feat.shape))
         output = feat
         return output
+
 
 def build_network(net_cfg):
     return Model(**net_cfg)
